bf77396855cd885bf42d66f6d3f1d296f18ea183
VladMatylevich <arazimga@yandex.ru>
2023-03-11 13:26:33 +0300

    Homework3

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte a = 67;
+        System.out.println("Значение переменной byte с типом a равно 67");
+        short b = 27897;
+        System.out.println("Значение переменной short с типом b равно 27897");
+        int c = 569;
+        System.out.println("Значение переменной int с типом c равно 569");
+        float e = 27.12f;
+        System.out.println("Значение переменной float с типом e равно 27.12f");
+        double i = 2.786;
+        System.out.println("Значение переменной double с типом i равно 2.786");
+        long bb = 987678965549L;
+        System.out.println("Значение переменной long с типом bb равно 987678965549L");
+
+    }
+}
